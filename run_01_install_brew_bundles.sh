0e0ea02d9657e2f3a0ca98cc5b3968cbce7e2a0f
Chris Guest <chrisguest75@users.noreply.github.com>
2023-01-15 21:46:48 +0000

    Add some paths

--- run_01_install_brew_bundles.sh
+++ run_01_install_brew_bundles.sh
@@ -6,6 +6,12 @@ echo "***********************************"
 echo "** Install Brew Bundle"
 echo "***********************************"
 
+# Homebrew
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -d /opt/homebrew && eval "$(/opt/homebrew/bin/brew shellenv)"
+test -f /usr/local/bin/brew && eval "$(/usr/local/bin/brew shellenv)"
+
 # Configure brew bundle
 export HOMEBREW_BUNDLE_FILE="$HOME/.BrewfileContainer"
 export HOMEBREW_BUNDLE_NO_LOCK=true
